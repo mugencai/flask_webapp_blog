915d41583ae1af6c79a40b8d1a14bba4a54738e9
mugencai <taikaidai233@yahoo.co.jp>
2022-05-01 14:10:35 +0900

    Initial commit

---
+++ flaskblog/__init__.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'zX\xf0i\xa9rd\xf9\xc6\xf4%N@\x97\x11pt\x8f\xe1\x9a?\xacr\xcc'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
+
+
+from flaskblog import routes
+
+
